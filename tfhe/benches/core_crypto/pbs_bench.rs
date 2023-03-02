c6730cdc5526df5c61ad457960f9b9f0ab719b3b
David Testé <david.teste@zama.ai>
2023-03-02 15:11:50 +0100

    chore(ci): use all available cpus to benchmarks pbs operation
    Benchmarks PBS at CPUs saturation is closer to real use case
    scenario.

--- tfhe/benches/core_crypto/pbs_bench.rs
+++ tfhe/benches/core_crypto/pbs_bench.rs
@@ -1,9 +1,12 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rayon::prelude::*;
 use tfhe::boolean::parameters::{BooleanParameters, DEFAULT_PARAMETERS, TFHE_LIB_PARAMETERS};
 use tfhe::core_crypto::prelude::*;
 use tfhe::shortint::keycache::NamedParam;
 use tfhe::shortint::parameters::*;
 use tfhe::shortint::Parameters;
+
+const VECTOR_SCALING_FACTOR: usize = 10;
 
 const SHORTINT_BENCH_PARAMS: [Parameters; 15] = [
     PARAM_MESSAGE_1_CARRY_0,
@@ -29,9 +32,9 @@ const BOOLEAN_BENCH_PARAMS: [(&str, BooleanParameters); 2] = [
 ];
 
 criterion_group!(
-    name = pbs_group;
-    config = Criterion::default().sample_size(2000);
-    targets = mem_optimized_pbs::<u64>, mem_optimized_pbs::<u32>
+    pbs_group,
+    mem_optimized_pbs::<u32>,
+    mem_optimized_pbs::<u64>,
 );
 
 criterion_main!(pbs_group);
@@ -92,7 +95,7 @@ fn benchmark_parameters<Scalar: Numeric>() -> Vec<(String, BenchmarkPbsParameter
     }
 }
 
-fn mem_optimized_pbs<Scalar: UnsignedTorus + CastInto<usize>>(c: &mut Criterion) {
+fn mem_optimized_pbs<Scalar: UnsignedTorus + CastInto<usize> + Sync + Send>(c: &mut Criterion) {
     // Create the PRNG
     let mut seeder = new_seeder();
     let seeder = seeder.as_mut();
@@ -101,79 +104,104 @@ fn mem_optimized_pbs<Scalar: UnsignedTorus + CastInto<usize>>(c: &mut Criterion)
     let mut secret_generator =
         SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
 
-    for (name, params) in benchmark_parameters::<Scalar>().iter() {
-        // Create the LweSecretKey
-        let input_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
-            params.input_lwe_dimension,
-            &mut secret_generator,
-        );
-        let output_glwe_secret_key: GlweSecretKeyOwned<Scalar> =
-            allocate_and_generate_new_binary_glwe_secret_key(
-                params.glwe_dimension,
-                params.polynomial_size,
+    for (cpus_kind, cpus_number) in (vec![
+        ("logical", num_cpus::get()),
+        ("physical", num_cpus::get_physical()),
+    ])
+    .iter()
+    {
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(*cpus_number)
+            .build()
+            .unwrap();
+
+        for (name, params) in benchmark_parameters::<Scalar>().iter() {
+            // Create the LweSecretKey
+            let input_lwe_secret_key = allocate_and_generate_new_binary_lwe_secret_key(
+                params.input_lwe_dimension,
                 &mut secret_generator,
             );
-        let output_lwe_secret_key = output_glwe_secret_key.into_lwe_secret_key();
+            let output_glwe_secret_key: GlweSecretKeyOwned<Scalar> =
+                allocate_and_generate_new_binary_glwe_secret_key(
+                    params.glwe_dimension,
+                    params.polynomial_size,
+                    &mut secret_generator,
+                );
+            let output_lwe_secret_key = output_glwe_secret_key.into_lwe_secret_key();
 
-        // Create the empty bootstrapping key in the Fourier domain
-        let fourier_bsk = FourierLweBootstrapKey::new(
-            params.input_lwe_dimension,
-            params.glwe_dimension.to_glwe_size(),
-            params.polynomial_size,
-            params.decomp_base_log,
-            params.decomp_level_count,
-        );
+            // Create the empty bootstrapping key in the Fourier domain
+            let fourier_bsk = FourierLweBootstrapKey::new(
+                params.input_lwe_dimension,
+                params.glwe_dimension.to_glwe_size(),
+                params.polynomial_size,
+                params.decomp_base_log,
+                params.decomp_level_count,
+            );
 
-        // Allocate a new LweCiphertext and encrypt our plaintext
-        let lwe_ciphertext_in: LweCiphertextOwned<Scalar> = allocate_and_encrypt_new_lwe_ciphertext(
-            &input_lwe_secret_key,
-            Plaintext(Scalar::ZERO),
-            params.lwe_modular_std_dev,
-            &mut encryption_generator,
-        );
+            let accumulator = GlweCiphertext::new(
+                Scalar::ZERO,
+                params.glwe_dimension.to_glwe_size(),
+                params.polynomial_size,
+            );
 
-        let accumulator = GlweCiphertext::new(
-            Scalar::ZERO,
-            params.glwe_dimension.to_glwe_size(),
-            params.polynomial_size,
-        );
+            let fft = Fft::new(fourier_bsk.polynomial_size());
+            let fft = fft.as_view();
 
-        // Allocate the LweCiphertext to store the result of the PBS
-        let mut out_pbs_ct = LweCiphertext::new(
-            Scalar::ZERO,
-            output_lwe_secret_key.lwe_dimension().to_lwe_size(),
-        );
+            let capacity = *cpus_number * VECTOR_SCALING_FACTOR;
+            let mut input_vec: Vec<LweCiphertextOwned<Scalar>> = Vec::with_capacity(capacity);
+            let mut output_vec: Vec<LweCiphertextOwned<Scalar>> = Vec::with_capacity(capacity);
+            let mut buffers_vec: Vec<ComputationBuffers> = Vec::with_capacity(capacity);
+            for _ in 0..(input_vec.capacity()){
+                // Allocate new LweCiphertext and encrypt our plaintext
+                input_vec.push(allocate_and_encrypt_new_lwe_ciphertext(
+                    &input_lwe_secret_key,
+                    Plaintext(Scalar::ZERO),
+                    params.lwe_modular_std_dev,
+                    &mut encryption_generator,
+                ));
 
-        let mut buffers = ComputationBuffers::new();
+                // Allocate LweCiphertexts to store the result of the PBS
+                output_vec.push(LweCiphertext::new(
+                    Scalar::ZERO,
+                    output_lwe_secret_key.lwe_dimension().to_lwe_size(),
+                ));
 
-        let fft = Fft::new(fourier_bsk.polynomial_size());
-        let fft = fft.as_view();
-
-        buffers.resize(
-            programmable_bootstrap_lwe_ciphertext_mem_optimized_requirement::<Scalar>(
-                fourier_bsk.glwe_size(),
-                fourier_bsk.polynomial_size(),
-                fft,
-            )
-            .unwrap()
-            .unaligned_bytes_required(),
-        );
-
-        let id = format!("PBS_mem-optimized_{name}");
-        #[allow(clippy::unit_arg)]
-        {
-            c.bench_function(&id, |b| {
-                b.iter(|| {
-                    black_box(programmable_bootstrap_lwe_ciphertext_mem_optimized(
-                        &lwe_ciphertext_in,
-                        &mut out_pbs_ct,
-                        &accumulator.as_view(),
-                        &fourier_bsk,
+                let mut buffers = ComputationBuffers::new();
+                buffers.resize(
+                    programmable_bootstrap_lwe_ciphertext_mem_optimized_requirement::<Scalar>(
+                        fourier_bsk.glwe_size(),
+                        fourier_bsk.polynomial_size(),
                         fft,
-                        buffers.stack(),
-                    ))
-                })
-            });
+                    )
+                    .unwrap()
+                    .unaligned_bytes_required(),
+                );
+                buffers_vec.push(buffers);
+            }
+
+            let id = format!("PBS_mem-optimized_{name}_{cpus_number}_{cpus_kind}_cpus");
+            #[allow(clippy::unit_arg)]
+            {
+                c.bench_function(&id, |b| {
+                    b.iter(|| {
+                        input_vec
+                            .par_iter()
+                            .zip(output_vec.par_iter_mut())
+                            .zip(buffers_vec.par_iter_mut())
+                            .for_each(|((input_ct, output_ct), buffers)| {
+                                programmable_bootstrap_lwe_ciphertext_mem_optimized(
+                                    input_ct,
+                                    output_ct,
+                                    &accumulator.as_view(),
+                                    &fourier_bsk,
+                                    fft,
+                                    buffers.stack(),
+                                )
+                            });
+                        black_box(&output_vec);
+                    })
+                });
+            }
         }
     }
 }
