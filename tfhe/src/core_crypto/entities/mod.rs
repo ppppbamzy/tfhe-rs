6221e233362131688a25098751d7057cfdc7f786
Arthur Meyre <arthur.meyre@zama.ai>
2023-06-13 15:26:42 +0200

    chore(core): add crs to entites mod to have it available in the prelude

--- tfhe/src/core_crypto/entities/mod.rs
+++ tfhe/src/core_crypto/entities/mod.rs
@@ -50,6 +50,7 @@ pub use crate::core_crypto::fft_impl::fft64::crypto::ggsw::{
 };
 pub use crate::core_crypto::fft_impl::fft64::math::polynomial::FourierPolynomial;
 pub use cleartext::*;
+pub use crs_lwe_ciphertext::*;
 pub use ggsw_ciphertext::*;
 pub use ggsw_ciphertext_list::*;
 pub use glwe_ciphertext::*;
