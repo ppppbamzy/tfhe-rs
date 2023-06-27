2fe635b7374e48f63be5f3a6d436a481435b5b8b
Benoit AVRIL <benoit.avril@zama.ai>
2023-06-27 17:23:24 +0200

    fine tuning

--- tfhe/src/core_crypto/algorithms/test/crs_lwe_modulo_switch.rs
+++ tfhe/src/core_crypto/algorithms/test/crs_lwe_modulo_switch.rs
@@ -160,7 +160,7 @@ fn test_covariance(dimension:usize,error: f64)->(f64,f64,f64,f64,f64,f64,f64,f64
         *el = (*el).wrapping_add((1 as u64)<<delta);
     }
     // Create a CRSLweCiphertext list
-    let number:usize= 1000;
+    let number:usize= 100000;
     let mut ciph_list =vec![CRSLweCiphertext::new(0u64, crs_lwe_dimension.to_crs_lwe_size(crs_lwe_codimension), ciphertext_modulus);number];
     //0..number.map(|_|).collect())
     let old_mod:usize = 64;
@@ -255,7 +255,7 @@ fn testy(){
 #[test]
 fn test_cov(){
     
-    for j in 25..=64{
+    for j in 25..=30{
         //let error = 1.0/((1<<j) as f64);
         let error = 2.0.powi(-(j as i32));
         //let error = 0.0;
@@ -269,17 +269,20 @@ fn test_cov(){
             let (cov,corr, sigma_x, sigma_y, var_x, var_y,expected,expected_cov,expected_var) = test_covariance(i,error);
             println!("cov: {}",cov);
             println!("expected_cov: {}",expected_cov);
-            println!("corr: {}",corr);
-            println!("expected_var: {}",expected_var);
-            println!("expected_log_2 var: {}",expected_var.log2());
-            println!("expected: {}",expected);
             println!("log_2 cov: {}",cov.log2());
             println!("log_2 expected_cov: {}",expected_cov.log2());
+            println!("corr: {}",corr);
+            println!("expected_corr: {}",expected);
+            println!("expected_var: {}",expected_var);
+            println!("var_x: {}",var_x);
+            println!("var_y: {}",var_y);
+            println!("expected_log_2 var: {}",expected_var.log2());
+            println!("log_2 var_x: {}",var_x.log2());
+            println!("log_2 var_y: {}",var_y.log2());
             
             //println!("sigma_x: {}",sigma_x);
             //println!("sigma_y: {}",sigma_y);
-            println!("log_2 var_x: {}",var_x.log2());
-            println!("log_2 var_y: {}",var_y.log2());
+
         }    
     }
     panic!();
